312899f8b584538205ca4460c9c2708422e9896d
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2022-12-18 14:55:41 +0100

    add concurrency

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,16 @@
-public class Calculator {
+public class Calculator implements Runnable {
+    public boolean isInterrupted = true;
+
+    @Override
+    public void run() {
+
+    }
+
+    public boolean getIsInterruptedStatus() {
+        return isInterrupted;
+    }
+
+    public void setInterruptedToFalse() {
+        this.isInterrupted = false;
+    }
 }
--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,2 +1,19 @@
 public class Demo {
+    public static void main(String[] args) {
+        Calculator calculatorDemo = new Calculator();
+        Thread demoThread = new Thread(calculatorDemo);
+
+        while (calculatorDemo.getIsInterruptedStatus()) {
+            System.out.println("Hello world!");
+
+            try {
+                demoThread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("This is my first pet-project named by \"Calculator\"");
+            calculatorDemo.setInterruptedToFalse();
+        }
+    }
 }
