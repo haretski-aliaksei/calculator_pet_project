cec8dceb1507bf780652359e5b7b6a0562b6bb79
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-01-06 13:54:19 +0100

    update pom.xml, add produceAddingNumbers method, add produceAddingNumbersTest

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator implements Runnable {
     public boolean isRun = true;
 
+    public double produceAddingNumbers(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
     @Override
     public void run() {
 
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculatorTest = new Calculator();
+
+    static Stream<Arguments> provideArgumentsForProduceAddingNumbersTest() {
+        return Stream.of(
+                Arguments.of(1, 99, 100),
+                Arguments.of(0, 5, 5)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("provideArgumentsForProduceAddingNumbersTest")
+    public void produceAddingNumbersTest(int firstParameter, int secondParameter, int expected) {
+        double actual = new Calculator().produceAddingNumbers(firstParameter, secondParameter);
+        assertEquals(expected, actual);
+    }
+}
