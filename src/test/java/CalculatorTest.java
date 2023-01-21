ed639a96aae204953000deb76a78d002e061961d
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-01-21 20:38:34 +0100

    update "CalculatorTest" class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,3 @@
-import org.junit.Before;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,20 +7,41 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    public Calculator calculatorTest;
-    @Before
 
-    static Stream<Arguments> provideArgumentsForProduceAddingNumbersTest() {
+    static Stream<Arguments> provideArgumentsForProduceAddingOfNumbersTest() {
         return Stream.of(
                 Arguments.of(1, 99, 100),
-                Arguments.of(0, 5, 5)
+                Arguments.of(0, -7, -7),
+                Arguments.of(-9, 0, -9),
+                Arguments.of(5, 0, 5),
+                Arguments.of(0, 5, 5),
+                Arguments.of(0, 0, 0)
         );
     }
 
     @ParameterizedTest()
-    @MethodSource("provideArgumentsForProduceAddingNumbersTest")
-    public void produceAddingNumbersTest(int firstParameter, int secondParameter, int expected) {
-        double actual = new Calculator().produceAddingOfNumbers(firstParameter, secondParameter);
+    @MethodSource("provideArgumentsForProduceAddingOfNumbersTest")
+    public void produceAddingOfNumbersTest(int firstParameter, int secondParameter, long expected) {
+        long actual = new Calculator().produceAddingOfNumbers(firstParameter, secondParameter);
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsProduceSubtractionOfNumbers() {
+        return Stream.of(
+                Arguments.of(100, 1, 99),
+                Arguments.of(1, 100, -99),
+                Arguments.of(-5, 10, -15),
+                Arguments.of(-3, -2, -1),
+                Arguments.of(-5, 0, -5),
+                Arguments.of(0, -7, 7),
+                Arguments.of(0, 0, 0)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("provideArgumentsProduceSubtractionOfNumbers")
+    public void produceSubtractionOfNumbersTest(int firstParameter, int secondParameter, int expected) {
+        int actual = new Calculator().produceSubtractionOfNumbers(firstParameter, secondParameter);
         assertEquals(expected, actual);
     }
 }
