eef70b85e251889502f7539071f57821055c7dce
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-01-21 22:42:52 +0100

    update "CalculatorTest" class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigInteger;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -43,5 +44,42 @@ public class CalculatorTest {
     public void produceSubtractionOfNumbersTest(int firstParameter, int secondParameter, int expected) {
         int actual = new Calculator().produceSubtractionOfNumbers(firstParameter, secondParameter);
         assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsProduceMultiplicationOfNumbers() {
+        return Stream.of(
+                Arguments.of(2, 2, BigInteger.valueOf(4)),
+                Arguments.of(0, 3, BigInteger.valueOf(0)),
+                Arguments.of(5, 0, BigInteger.valueOf(0)),
+                Arguments.of(-5, 1, BigInteger.valueOf(-5)),
+                Arguments.of(6, -1, BigInteger.valueOf(-6)),
+                Arguments.of(-7, -1, BigInteger.valueOf(7)),
+                Arguments.of(0, 0, BigInteger.valueOf(0))
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("provideArgumentsProduceMultiplicationOfNumbers")
+    public void produceMultiplicationOfNumbersTest(int firstParameter, int secondParameter, BigInteger expected) {
+        BigInteger actual = new Calculator().produceMultiplicationOfNumbers(firstParameter, secondParameter);
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsProduceDivisionOfNumbers() {
+        return Stream.of(
+                Arguments.of(10, 4, 2.5),
+                Arguments.of(2, 1, 2.0),
+                Arguments.of(3, -1, -3.0),
+                Arguments.of(-4, 1, -4.0),
+                Arguments.of(-10, -2, 5.0),
+                Arguments.of(0, 10, 0.0)
+        );
+    }
+
+    @ParameterizedTest()
+    @MethodSource("provideArgumentsProduceDivisionOfNumbers")
+    public void produceDivisionOfNumbersTest(int firstParameter, int secondParameter, double expected) {
+        double actual = new Calculator().produceDivisionOfNumbers(firstParameter, secondParameter);
+        assertEquals(expected, actual, 0.01);
     }
 }
